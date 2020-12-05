881700819bbd3655f716e09e68f89d2fa4925901
WolFoxHD <60907817+WolFoxHD@users.noreply.github.com>
2020-12-05 11:39:03 +0700

    Main.php
    Update English Only

--- src/Broadcast/Main.php
+++ src/Broadcast/Main.php
@@ -15,21 +15,21 @@ class main extends PluginBase implements Listener
 
     public function onEnable()
     {
-        $this->getLogger()->info(TextFormat::YELLOW."Broadcast Plugin wurde Aktiviert.");
+        $this->getLogger()->info(TextFormat::YELLOW."Broadcast plugin has been activated.");
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
     {
         if($cmd->getName() === "broadcast"){
             if(!$sender->hasPermission("broadcast.cmd")){
-                $sender->sendMessage(TextFormat::RED."Keine Rechte!");
+                $sender->sendMessage(TextFormat::RED."No right!");
             }else{
                 if(isset($args[0])){
                     $msg = implode(" ", $args);
                     $this->getServer()->broadcastMessage(TextFormat::AQUA.$msg);
-                    $sender->sendMessage(TextFormat::AQUA."Die Nachricht ".$msg." Wurde an den Server Gesendet.");
+                    $sender->sendMessage(TextFormat::AQUA."The message ".$msg." Has been sent to the server.");
                 }else{
-                    $sender->sendMessage(TextFormat::RED."Benutzung: /broadcast, /bc Deine Nachricht");
+                    $sender->sendMessage(TextFormat::RED."use: /broadcast, /bc Your message");
                 }
             }
         }
